e64881421d21cc7a245ba81a8d1f624fec1dfde2
James Grantham <james.r.grantham@gmail.com>
2019-08-19 12:04:09 +0100

    First intern added

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,7 +18,12 @@ const example = {
 }
 
 // Write your intern objects here:
-
+const Mitzi = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F"
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
