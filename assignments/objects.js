8a497cff23cd6a1f4b0099cd7fae0b2a9c180e0b
James Grantham <james.r.grantham@gmail.com>
2019-08-19 12:54:47 +0100

    Add function multiply numbers

--- assignments/objects.js
+++ assignments/objects.js
@@ -52,7 +52,9 @@ const Antonietta = {
   name: "Antonietta",
   email: "adaine5@samsung.com",
   gender: "F",
-  multiplyNums: ""
+  multiplyNums: function (num1, num2) {
+    return num1*num2
+  }
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
@@ -71,10 +73,10 @@ console.log(Antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-Kennan.speak
+Kennan.speak;
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(Antonietta.multiplyNums(3,4));
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
