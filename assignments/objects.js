bd4e966786ef2af6afdb574be4d1175a5f07a683
James Grantham <james.r.grantham@gmail.com>
2019-08-19 12:18:17 +0100

    Complete HR requests

--- assignments/objects.js
+++ assignments/objects.js
@@ -22,50 +22,51 @@ const Mitzi = {
   id: 1,
   name: "Mitzi",
   email: "mmelloy0@psu.edu",
-  gender: "F"
+  gender: "F",
 }
 
 const Kennan = {
   id: 2,
   name: "Kennan",
   email: "kdiben1@tinypic.com",
-  gender: "M"
+  gender: "M",
+  speak: 'F'
 }
 
 const Keven = {
   id: 3,
   name: "Keven",
   email: "kmummery2@wikimedia.org",
-  gender: "M"
+  gender: "M",
 }
 
 const Gannie = {
   id: 4,
   name: "Gannie",
   email: "gmartinson3@illinois.edu",
-  gender: "M"
+  gender: "M",
 }
 
 const Antonietta = {
   id: 5,
   name: "Antonietta",
   email: "adaine5@samsung.com",
-  gender: "F"
+  gender: "F",
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(Mitzi.name)
 // Kennan's ID
-
+console.log(Kennan.id)
 // Keven's email
-
+console.log(Keven.email)
 // Gannie's name
-
+console.log(Gannie.name)
 // Antonietta's Gender
-
+console.log(Antonietta.gender)
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
